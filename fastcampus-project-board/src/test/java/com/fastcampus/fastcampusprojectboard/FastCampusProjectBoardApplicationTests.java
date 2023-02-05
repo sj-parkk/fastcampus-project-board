9618bf60896bfe4942b3a644af36302a2e10cbde
sjpark <psjjj3408@gmail.com>
2023-02-05 14:52:10 +0900

    #6- 스프링부트 프로젝트 시
    * 스프링 웹
    * devtools
    * lombok

---
+++ fastcampus-project-board/src/test/java/com/fastcampus/fastcampusprojectboard/FastCampusProjectBoardApplicationTests.java
@@ -0,0 +1,14 @@
+package com.fastcampus.fastcampusprojectboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FastCampusProjectBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+		System.out.println("테스트");
+	}
+
+}
